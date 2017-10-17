148e244bc363d59c8b2c6caf09e1bdb416f60cae
KeltarHelviett <keltarhelviett@gmail.com>
2017-10-18 00:48:48 +1000

    Added view for 2nd lab (fall modeling)

---
+++ MKM_Labs/MKM_Labs/MathUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKM_Labs
+{
+    static class MathUtils
+    {
+        public static List<double> EulerCromer(IEnumerable<double> xs)
+        {
+            return null;
+        }
+    }
+}
