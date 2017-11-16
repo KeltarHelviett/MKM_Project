ae824a1dcc7ffa2a4db2d25d5860735fbc145187
KeltarHelviett <keltarhelviett@gmail.com>
2017-11-16 20:18:40 +1000

    Added View and binded to it's VM for Harmonic Oscillator Modeling lab

---
+++ MKM_Labs/MKM_Labs/ViewModels/HarmonicOscillatorModeling/HarmonicOscillatorModelingViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKM_Labs.ViewModels.HarmonicOscillatorModeling
+{
+    class HarmonicOscillatorModelingViewModel: INotifyPropertyChanged
+    {
+        #region PublicProperties
+
+        private double friction;
+
+        public double Friction
+        {
+            get { return friction; }
+            set
+            {
+                friction = value;
+                OnPropertyChanged(nameof(Friction));
+            }
+        }
+
+        private double rigidity;
+
+        public double Rigidity
+        {
+            get { return rigidity; }
+            set
+            {
+                rigidity = value;
+                OnPropertyChanged(nameof(Rigidity));
+            }
+        }
+
+        private double mass;
+
+        public double Mass
+        {
+            get { return mass; }
+            set
+            {
+                mass = value;
+                OnPropertyChanged(nameof(Mass));
+            }
+        }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MKM_Labs/MKM_Labs/Views/HarmonicOscillatorModeling/HarmonicOscillatorModelingView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MKM_Labs.ViewModels.HarmonicOscillatorModeling;
+
+namespace MKM_Labs.Views.HarmonicOscillatorModeling
+{
+    public partial class HarmonicOscillatorModelingView : Window
+    {
+        public HarmonicOscillatorModelingView()
+        {
+            InitializeComponent();
+            DataContext = new HarmonicOscillatorModelingViewModel();
+        }
+    }
+}
