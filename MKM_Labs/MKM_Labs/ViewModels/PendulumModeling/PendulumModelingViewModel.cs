976cb61096d377cbf0686dd3d346a3273d31c309
KeltarHelviett <keltarhelviett@gmail.com>
2017-12-14 21:23:19 +1000

    Added Pendulum Modeling View and ViewModel

---
+++ MKM_Labs/MKM_Labs/ViewModels/PendulumModeling/PendulumModelingViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKM_Labs.ViewModels.PendulumModeling
+{
+    class PendulumModelingViewModel: INotifyPropertyChanged
+    {
+
+        #region PublicProperties
+
+        private bool isSmallAngleEquation = true;
+
+        public bool IsSmallAngleEquation
+        {
+            get { return isSmallAngleEquation; }
+            set
+            {
+                isSmallAngleEquation = value;
+                OnPropertyChanged(nameof(IsSmallAngleEquation));
+            }
+        }
+
+        private bool isAnyAngleEquation;
+
+        public bool IsAnyAngleEquation
+        {
+            get { return isAnyAngleEquation; }
+            set
+            {
+                isAnyAngleEquation = value;
+                OnPropertyChanged(nameof(IsAnyAngleEquation));
+            }
+        }
+
+        private double barLength = 1; // Длина стержня L
+
+        public double BarLength
+        {
+            get { return barLength; }
+            set
+            {
+                barLength = value;
+                OnPropertyChanged(nameof(BarLength));
+            }
+        }
+
+        private double initialSpeed = 2; // Нач. скорость V₀
+
+        public double InitialSpeed
+        {
+            get { return initialSpeed; }
+            set
+            {
+                initialSpeed = value;
+                OnPropertyChanged(nameof(InitialSpeed));
+            }
+        }
+
+        private double initialAngle = 3; // Нач. угол θ₀
+
+        public double InitialAngle
+        {
+            get { return initialAngle; }
+            set
+            {
+                initialAngle = value;
+                OnPropertyChanged(nameof(InitialAngle));
+            }
+        }
+
+        private double mass = 3;
+
+        public double Mass
+        {
+            get { return mass; }
+            set
+            {
+                mass = value;
+                OnPropertyChanged(nameof(Mass));
+            }
+        }
+
+        private double constU = 3; // Постоянная компонента скорости среды U_const
+
+        public double ConstU
+        {
+            get { return constU; }
+            set
+            {
+                constU = value;
+                OnPropertyChanged(nameof(ConstU));
+            }
+        }
+
+        private double initialEnvSpeed = 3; // U₀ нач. скорость среды
+
+        public double InitialEnvSpeed
+        {
+            get { return initialEnvSpeed; }
+            set
+            {
+                initialEnvSpeed = value;
+                OnPropertyChanged(nameof(InitialEnvSpeed));
+            }
+        }
+
+        private double harmonicFrequency = 8; // Ω₀ переменная (гармоническая) частота
+
+        public double HarmonicFrequency
+        {
+            get { return harmonicFrequency; }
+            set
+            {
+                harmonicFrequency = value;
+                OnPropertyChanged(nameof(HarmonicFrequency));
+            }
+        }
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Calculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MKM_Labs/MKM_Labs/Views/PendulumModeling/PendulumModelingView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MKM_Labs.ViewModels.PendulumModeling;
+
+namespace MKM_Labs.Views.PendulumModeling
+{
+    public partial class PendulumModelingView : Window
+    {
+        public PendulumModelingView()
+        {
+            InitializeComponent();
+            DataContext = new PendulumModelingViewModel();
+        }
+
+        private void CalculateBtnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
