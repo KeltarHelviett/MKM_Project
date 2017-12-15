30a830e5c64505242bdc46524f6eb6e493ba7984
Oleg Prikhodko <oldmpr@rambler.ru>
2017-12-15 15:20:16 +1000

    Added wind

--- MKM_Labs/MKM_Labs/MathUtils.cs
+++ MKM_Labs/MKM_Labs/MathUtils.cs
@@ -9,33 +9,35 @@ namespace MKM_Labs
     static class MathUtils
     {
         private static Tuple<List<double>, List<double>, List<double>, List<double>> EulerCromer(List<double> ts,
-            double y0, double v0, Func<double, double, double, double> a, Func<double, double, double> e)
+            double y0, double v0, Func<double, double, double, double> a, Func<double, double, double> e,
+            Func<double, double> Vsr)
         {
             var ys = new List<double> { Capacity = ts.Count() };
             ys.Add(y0);
             var vs = new List<double> { Capacity = ts.Count() };
-            vs.Add(v0);
+            vs.Add(v0 + Vsr(0));
             var es = new List<double> { Capacity = ts.Count() };
             es.Add(e(y0, v0));
             for (int i = 0; i < ts.Count() - 1; ++i)
             {
                 var deltat = ts[i + 1] - ts[i];
-                vs.Add(vs[i] + deltat * a(ys[i], vs[i], deltat));
-                ys.Add(ys[i] + deltat * vs[i + 1]);
-                es.Add(e(ys[i + 1], vs[i + 1]));
+                vs.Add( vs[i] + deltat * a(ys[i], vs[i], deltat) + Vsr(i * deltat) );
+                ys.Add( ys[i] + deltat * vs[i + 1] );
+                es.Add( e(ys[i + 1], vs[i + 1]) );
             }
             return Tuple.Create(ts, ys, vs, es);
         }
 
         public static Tuple<List<double>, List<double>, List<double>, List<double>> EulerCromer(double t0, double endt,
-            double steporn, bool isStep, double y0, double v0, Func<double, double, double, double> a, Func<double, double, double> e)
+            double steporn, bool isStep, double y0, double v0, Func<double, double, double, double> a, Func<double, double, double> e,
+            Func<double, double> vsr)
         {
             if (!isStep)
                 steporn = (endt - t0) / (steporn - 1);
             var ts = new List<double>();
             for (var i = 0; steporn * i <= endt; ++i)
                 ts.Add(steporn * i);
-            return EulerCromer(ts, y0, v0, a, e);
+            return EulerCromer(ts, y0, v0, a, e, vsr);
         }
     }
 }
